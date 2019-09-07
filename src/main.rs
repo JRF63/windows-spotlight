cd063097756f730dc8712be8bc53542104efee1c
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2019-09-07 14:15:46 +0800

    Split the hashing into an update and digest.

--- src/main.rs
+++ src/main.rs
@@ -32,58 +32,64 @@ impl WinHasher {
         let mut helper_fn = || -> Result<(), i32> {
             let mut data = 0;
 
-            match unsafe { bcrypt::BCryptOpenAlgorithmProvider(
-                &mut alg_handle,
-                to_wchar(hash_id).as_ptr(),
-                std::ptr::null_mut(),
-                bcrypt::BCRYPT_HASH_REUSABLE_FLAG)
+            match unsafe {
+                bcrypt::BCryptOpenAlgorithmProvider(
+                    &mut alg_handle,
+                    to_wchar(hash_id).as_ptr(),
+                    std::ptr::null_mut(),
+                    bcrypt::BCRYPT_HASH_REUSABLE_FLAG)
             } {
                 ntstatus::STATUS_SUCCESS => rollback += 1,
                 e => return Err(e)
             };
 
             let mut hash_data_size: u32 = 0;
-            match unsafe { bcrypt::BCryptGetProperty(
-                alg_handle,
-                to_wchar(bcrypt::BCRYPT_OBJECT_LENGTH).as_ptr(),
-                (&mut hash_data_size as *mut u32) as *mut u8,
-                32, // size of DWORD
-                &mut data,
-                0)
+            match unsafe {
+                bcrypt::BCryptGetProperty(
+                    alg_handle,
+                    to_wchar(bcrypt::BCRYPT_OBJECT_LENGTH).as_ptr(),
+                    (&mut hash_data_size as *mut u32) as *mut u8,
+                    32, // size of DWORD
+                    &mut data,
+                    0)
             } {
-                ntstatus::STATUS_SUCCESS => (),
+                ntstatus::STATUS_SUCCESS => {
+                    hash_data.reserve_exact(hash_data_size as usize);
+                    unsafe{ hash_data.set_len(hash_data_size as usize) };
+                },
                 e => return Err(e)
             };
-            hash_data.reserve_exact(hash_data_size as usize);
-            unsafe{ hash_data.set_len(hash_data_size as usize) };
 
-            match unsafe { bcrypt::BCryptCreateHash(
-                alg_handle,
-                &mut hash_handle,
-                hash_data.as_mut_ptr(),
-                hash_data_size.into(),
-                std::ptr::null_mut(),
-                0,
-                0)
+            match unsafe {
+                bcrypt::BCryptCreateHash(
+                    alg_handle,
+                    &mut hash_handle,
+                    hash_data.as_mut_ptr(),
+                    hash_data_size.into(),
+                    std::ptr::null_mut(),
+                    0,
+                    0)
             } {
                 ntstatus::STATUS_SUCCESS => rollback += 1,
                 e => return Err(e)
             };
 
             let mut hash_result_size: u32 = 0;
-            match unsafe { bcrypt::BCryptGetProperty(
-                alg_handle,
-                to_wchar(bcrypt::BCRYPT_HASH_LENGTH).as_ptr(),
-                (&mut hash_result_size as *mut u32) as *mut u8,
-                32, // size of DWORD
-                &mut data,
-                0)
+            match unsafe {
+                bcrypt::BCryptGetProperty(
+                    alg_handle,
+                    to_wchar(bcrypt::BCRYPT_HASH_LENGTH).as_ptr(),
+                    (&mut hash_result_size as *mut u32) as *mut u8,
+                    32, // size of DWORD
+                    &mut data,
+                    0)
             } {
-                ntstatus::STATUS_SUCCESS => (),
+                ntstatus::STATUS_SUCCESS => {
+                    hash_result.reserve_exact(hash_result_size as usize);
+                    unsafe{ hash_result.set_len(hash_result_size as usize) };
+                },
                 e => return Err(e)
             };
-            hash_result.reserve_exact(hash_result_size as usize);
-            unsafe{ hash_result.set_len(hash_result_size as usize) };
 
             Ok(())
         };
@@ -111,37 +117,31 @@ impl WinHasher {
         }
     }
 
-    pub fn hash_object<T>(&mut self, object: &mut [T]) -> Result<Vec<u8>, i32> {
-        let mut inner_fn = || -> Result<(), i32> {
-            match unsafe { bcrypt::BCryptHashData(
+    pub fn update<T>(&mut self, object: &mut [T]) -> Result<(), i32> {
+        match unsafe {
+            bcrypt::BCryptHashData(
                 self.hash_handle,
                 object.as_mut_ptr() as *mut u8,
                 (object.len() * std::mem::size_of::<T>()) as u32,
                 0)
-            } {
-                ntstatus::STATUS_SUCCESS => (),
-                e => return Err(e)
-            };
+        } {
+            ntstatus::STATUS_SUCCESS => Ok(()),
+            e => return Err(e)
+        }
+    }
 
-            match unsafe { bcrypt::BCryptFinishHash(
+    pub fn digest(&mut self) -> Result<Vec<u8>, i32> {
+        match unsafe {
+            bcrypt::BCryptFinishHash(
                 self.hash_handle,
                 self.hash_result.as_mut_ptr(),
                 self.hash_result.len() as u32,
                 0)
-            } {
-                ntstatus::STATUS_SUCCESS => (),
-                e => return Err(e)
-            };
-
-            Ok(())
-        };
-
-        match inner_fn() {
-            Ok(_) => {
-                let duplicate = self.hash_result.clone();
-                Ok(duplicate)
+        } {
+            ntstatus::STATUS_SUCCESS => {
+                Ok(self.hash_result.clone())
             },
-            Err(e) => Err(e)
+            e => return Err(e)
         }
     }
 }
@@ -160,26 +160,33 @@ fn main() {
     let mut msg2: Vec<u8> = vec![0x61, 0x62, 0x63];
     let mut msg3 = String::from("hello");
     let mut hasher = WinHasher::new(bcrypt::BCRYPT_SHA256_ALGORITHM).expect("Failed to create hasher");
-    if let Ok(result) = hasher.hash_object(&mut msg1) {
-        for i in result {
-            print!{"{:02x}", i}
-        }
-        println!("");
-    }
-    if let Ok(result) = hasher.hash_object(&mut msg2) {
-        for i in result {
-            print!{"{:02x}", i}
-        }
-        println!("");
-    }
     
-    unsafe {
-        let mut slice = std::slice::from_raw_parts_mut(msg3.as_mut_ptr(), msg3.len());
-        if let Ok(result) = hasher.hash_object(&mut slice) {
+    if let Ok(()) = &hasher.update(&mut msg1) {
+        if let Ok(result) = &hasher.digest() {
             for i in result {
                 print!{"{:02x}", i}
             }
             println!("");
+        }
+    }
+    if let Ok(()) = &hasher.update(&mut msg2) {
+        if let Ok(result) = &hasher.digest() {
+            for i in result {
+                print!{"{:02x}", i}
+            }
+            println!("");
+        }
+    }
+    
+    unsafe {
+        let mut slice = std::slice::from_raw_parts_mut(msg3.as_mut_ptr(), msg3.len());
+        if let Ok(()) = &hasher.update(&mut slice) {
+            if let Ok(result) = &hasher.digest() {
+                for i in result {
+                    print!{"{:02x}", i}
+                }
+                println!("");
+            }
         }
     }
 }
