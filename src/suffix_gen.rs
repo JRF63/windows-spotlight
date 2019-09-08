db98a34d4a773b440fd24d5e9fd18c4e82fd36e6
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2019-09-09 07:45:12 +0800

    Simplified SuffixGenerator iteration.

--- src/suffix_gen.rs
+++ src/suffix_gen.rs
@@ -2,77 +2,56 @@ use std::path::PathBuf;
 
 pub struct SuffixGenerator {
     path_array: Vec<u8>,
-    original_size: isize,
+    original_size: usize,
     extension: String,
-    array_pos: isize,
-    ascii_lower: Box<dyn Iterator<Item=u8>>
+    array_pos: usize,
 }
 
 impl SuffixGenerator {
     pub fn new(path: PathBuf, ext: &str) -> SuffixGenerator {
-        if let Ok(path_string) = path.into_os_string().into_string() {
-            let mut path_array: Vec<u8> = path_string.into_bytes();
-            let original_size = path_array.len() as isize;
-            let mut extension = String::from(".");
-            extension.push_str(ext);
-            let ascii_iter = Box::new((97u8..=122u8).chain(std::iter::once(0u8)).cycle());
-            return SuffixGenerator {
-                path_array,
-                original_size,
-                extension,
-                array_pos: -1,
-                ascii_lower: ascii_iter
-            };
-        } else {
-            panic!("Invalid unicode");
-        }
+        let mut path_array: Vec<u8> = path.into_os_string()
+                                          .into_string().expect("Invalid unicode")
+                                          .into_bytes();
+        let original_size = path_array.len() as usize;
+        path_array.push(96u8);
+
+        let mut extension = String::from(".");
+        extension.push_str(ext);
+        
+        return SuffixGenerator {
+            path_array,
+            original_size,
+            extension,
+            array_pos: original_size
+        };
     }
 
-    fn adjust_suffix(&mut self) {
-        // place in ::new
-        if self.array_pos == -1 {
-            self.path_array.push(96u8);
-            self.array_pos = 0;
-        }
-
-        let mut cur_pos = self.array_pos;
-        loop {
-            if cur_pos < 0 {
-                self.path_array.push(97u8);
-                self.array_pos += 1;
-                break;
-            }
-            let pos_offset = (cur_pos + self.original_size) as usize;
-            self.path_array[pos_offset] += 1;
-            if self.path_array[pos_offset] == 123u8 {
-                self.path_array[pos_offset] = 97;
-                cur_pos -= 1;
-            } else {
-                break;
-            }
-        }
+    fn make_pathbuf(&mut self) -> Option<PathBuf> {
+        let mut tmp_str: String = String::from_utf8(self.path_array.clone()).expect("Invalid unicode");
+        tmp_str.push_str(&self.extension);
+        Some(PathBuf::from(tmp_str))
     }
 }
 
 impl Iterator for SuffixGenerator {
     type Item = PathBuf;
     fn next(&mut self) -> Option<Self::Item> {
+        let mut cur_pos = self.array_pos;
         loop {
-            match self.ascii_lower.next() {
-                Some(0) => {
-                    self.adjust_suffix();
-                },
-                Some(ch) => {
-                    self.path_array.push(ch);
-                    
-                    let mut tmp_str: String = String::from_utf8(self.path_array.clone()).expect("Invalid unicode");
-                    tmp_str.push_str(&self.extension);
-                    let result = PathBuf::from(tmp_str);
+            if cur_pos < self.original_size {
+                self.path_array.push(97u8); // push an 'a'
+                self.array_pos += 1;
+                return self.make_pathbuf();
+            }
 
-                    self.path_array.pop(); // remove ch
-                    return Some(result);
-                },
-                None => panic!("Should be unreachable")
+            self.path_array[cur_pos] += 1;
+            // if current char is past 'z', make it an 'a' and
+            // carry over to next position
+            if self.path_array[cur_pos] == 123u8 {
+                self.path_array[cur_pos] = 97;
+                cur_pos -= 1;
+            } else {
+                return self.make_pathbuf();
             }
         }
     }
